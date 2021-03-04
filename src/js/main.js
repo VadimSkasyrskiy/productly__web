32248957dfc9982e663c13d65caebe65eeee4d48
VadimSkasyrskiy <v.skasurskii@gmail.com>
2021-03-04 22:14:30 +0300

    added adaptive for all screens

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,22 @@
 $('.marketing__slider').slick({
     dots: true,
+    arrows: false,
     centerMode: true,
     slidesToShow: 1,
     variableWidth: true,
-  });
+    responsive: [
+        {
+          breakpoint: 620,
+          settings: {
+            variableWidth: false,
+            centerMode: false,
+            slidesToShow: 1,
+          }
+        }
+      ]
+});
+
+$('.main-header__button-menu').on('click', function() {
+   $('.main-header__navigation').toggleClass('main-header--active');
+   $(this).toggleClass('main-header__button-menu--active');
+});
